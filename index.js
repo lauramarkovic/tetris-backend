964191fc8cc16edf4f7e87c260f13546e0484cbd
Laura Markovičová <markovicova.laura@gmail.com>
2021-04-18 14:30:47 +0200

    final fixes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config");
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -14,8 +16,11 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB Connected!');
-    return server.listen({ port: 5000 })
+    return server.listen({ port: PORT })
   })
   .then(res => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err);
   });
